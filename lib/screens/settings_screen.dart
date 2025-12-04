21cc2b4653b427bd34f8bf42b9335c6a5e787bdc
davcruman <davcruman@alu.edu.gva.es>
2025-12-04 17:01:25 +0100

    error

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,5 +1,7 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:app_settings/app_settings.dart';
+
 
 class SettingsScreen extends StatelessWidget {
   final Color accentColor = const Color(0xFF54FF78); 
@@ -141,31 +143,38 @@ class SettingsScreen extends StatelessWidget {
   // ------------------------------------------------------------
   // Tile con flecha
   // ------------------------------------------------------------
-  Widget _linkTile(IconData icon, String text) {
-    return InkWell(
-      onTap: () {},
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 14),
-        child: Row(
-          children: [
-            Icon(icon, color: Colors.white.withOpacity(0.95), size: 22),
-            const SizedBox(width: 16),
-            Expanded(
-              child: Text(
-                text,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                ),
+Widget _linkTile(IconData icon, String text) {
+  return InkWell(
+    onTap: () {
+      if (text == "Permisos de ubicación") {
+        AppSettings.openLocationSettings(); // abre los ajustes de ubicación
+      } else {
+        // manejar otras opciones
+      }
+    },
+    child: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 14),
+      child: Row(
+        children: [
+          Icon(icon, color: Colors.white.withOpacity(0.95), size: 22),
+          const SizedBox(width: 16),
+          Expanded(
+            child: Text(
+              text,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
               ),
             ),
-            const Icon(Icons.chevron_right_rounded, color: Colors.white24, size: 22),
-          ],
-        ),
+          ),
+          const Icon(Icons.chevron_right_rounded, color: Colors.white24, size: 22),
+        ],
       ),
-    );
-  }
+    ),
+  );
+}
+
 
   // ------------------------------------------------------------
   // Tile con switch
