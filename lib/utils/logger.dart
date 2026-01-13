c36da6394fb9e23d72b1b21184995ff4c816a66f
Ángel Sáez Díaz <angsaedia@alu.edu.gva.es>
2026-01-13 16:09:35 +0100

    Logger

--- lib/utils/logger.dart
+++ lib/utils/logger.dart
@@ -1,18 +1,18 @@
 import 'package:logger/logger.dart';
-import 'file_logger_output.dart'; // Importamos la clase de arriba
+import 'file_logger_output.dart'; 
 
 final logger = Logger(
   filter: ProductionFilter(),
   printer: PrettyPrinter(
     methodCount: 0,
-    errorMethodCount: 8, // Mostrar pila de llamadas en errores
+    errorMethodCount: 8, 
     lineLength: 120,
     colors: true,
     printEmojis: true,
     dateTimeFormat: DateTimeFormat.dateAndTime,
   ),
   output: MultiOutput([
-    ConsoleOutput(),    // Salida por consola de VS Code
-    FileLoggerOutput(), // Salida a los archivos app.log y error.log
+    ConsoleOutput(),    
+    FileLoggerOutput(), 
   ]),
 );
